c926eea3f3af3e3bc083d7d30b5a000cd21bcfc5
曾海明 <4041812@qq.com>
2019-06-10 18:38:53 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import {configInit, setENV, setBaseInfo} from './assets/envConfig.js'
 // import {getwxAuth} from '../src/service/authService.js'
+// import {wxConfig} from '../src/service/wxService.js'
 
 // 自动加载组件
 import './components'
@@ -14,9 +16,15 @@ import { Toast } from 'vant';
 Vue.use(Toast);
 
 Vue.config.productionTip = false
-
-new Vue({
-    router,
-    store,
-    render: h => h(App)
-}).$mount('#app')
+// 配置初始化
+// configInit();
+// 获取微信授权
+// getwxAuth().then(() => {
+   // wxConfig(location.href).then(() => {
+      new Vue({
+          router,
+          store,
+          render: h => h(App)
+      }).$mount('#app')
+   // })
+// });
