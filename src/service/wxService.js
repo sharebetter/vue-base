8a3374d3a8b61758e5409cefd4810cc8e9161eb2
曾海明 <>
2019-06-18 11:27:56 +0800

    添加微信授权验证

--- src/service/wxService.js
+++ src/service/wxService.js
@@ -13,7 +13,7 @@ export async function wxConfig (url) {
         timestamp: result.timestamp, // 必填，生成签名的时间戳
         nonceStr: result.nonceStr, // 必填，生成签名的随机串
         signature: result.signature, // 必填，签名
-		    jsApiList: ['onMenuShareAppMessage', 'onMenuShareTimeline', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone', 'chooseWXPay'] // 必填，需要使用的JS接口列表
+		    jsApiList: ['updateTimelineShareData', 'updateAppMessageShareData', 'onMenuShareWeibo', 'chooseWXPay'] // 必填，需要使用的JS接口列表
       })
       wx.ready(function () {
             resolve()
@@ -54,37 +54,25 @@ export function wxPay (WX_PUB) {
  */
 export function wxShare (title, link, imgUrl, desc) {
   return new Promise((resolve) => {
-    wx.onMenuShareTimeline({
+    wx.updateTimelineShareData({
       title: title, // 分享标题
       link: encodeURI(link), // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
       imgUrl: imgUrl, // 分享图标
       success: function () {
+        share(scene)
         resolve(true)
       },
       fail: function () {
         resolve(false)
       }
     });
-    wx.onMenuShareAppMessage({
+    wx.updateAppMessageShareData({
       title: title, // 分享标题
       desc: desc, // 分享描述
       link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
       imgUrl: imgUrl, // 分享图标
-      type: 'link', // 分享类型,music、video或link，不填默认为link
-      dataUrl: '',
       success: function () {
-        resolve(true)
-      },
-      cancel: function () {
-        resolve(false)
-      }
-    });
-    wx.onMenuShareQQ({
-      title: title, // 分享标题
-      desc: desc, // 分享描述
-      link: encodeURI(link), // 分享链接
-      imgUrl: imgUrl, // 分享图标
-      success: function () {
+        share(scene)
         resolve(true)
       },
       cancel: function () {
@@ -97,18 +85,7 @@ export function wxShare (title, link, imgUrl, desc) {
       link: encodeURI(link), // 分享链接
       imgUrl: imgUrl, // 分享图标
       success: function () {
-        resolve(true)
-      },
-      cancel: function () {
-        resolve(false)
-      }
-    });
-    wx.onMenuShareQZone({
-      title: title, // 分享标题
-      desc: desc, // 分享描述
-      link: encodeURI(link), // 分享链接
-      imgUrl: imgUrl, // 分享图标
-      success: function () {
+        share(scene)
         resolve(true)
       },
       cancel: function () {
